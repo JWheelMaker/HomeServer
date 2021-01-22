06a9a5e28160f5994daa0eaf4d1ee8f13cdc7c37
Jonas Radenacher <j.rademacher743@gmail.com>
2021-01-22 10:34:11 +0100

    - update.sh now updates all nextcloud apps

--- Docker-Compose/update.sh
+++ Docker-Compose/update.sh
@@ -22,6 +22,16 @@ do
 	fi
 done
 
+read -p "Should the Nextcloud cron-job be done now?(y/n)" answer
+    if [ "$answer" == "y" ]
+    then
+            cd nextcloud/
+            docker-compose exec --user www-data nextcloud-app php -f cron.php
+    fi
+
+cd nextcloud
+docker-compose exec --user www-data nextcloud-app php occ app:update --all
+
 echo
 echo "--------------------"
 echo "- Setup Complete ! -"
