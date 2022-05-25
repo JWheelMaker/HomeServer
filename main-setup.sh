828a036d3c1d279e576de61d5299bc3a5562b64d
Jonas <51298304+JWheelMaker@users.noreply.github.com>
2022-05-25 10:30:55 +0200

    docker-compose install via apt

--- main-setup.sh
+++ main-setup.sh
@@ -24,10 +24,7 @@ echo \
 sudo apt install ddclient
 
 #install docker compose
-DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
-mkdir -p $DOCKER_CONFIG/cli-plugins
-curl -SL https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-linux-aarch64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
-chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
+sudo apt-get instll docker-compose-plugin
 docker compose version
 
 #fixing permissions for nextcloud app folder
